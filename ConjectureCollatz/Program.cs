ca5bbc2969ed4ac770406601f75b5c03572b6da2
K <K@DIEGO>
2017-09-04 20:29:35 -0300

    Conjecture collatz initial

--- ConjectureCollatz/Program.cs
+++ ConjectureCollatz/Program.cs
@@ -10,6 +10,40 @@ namespace ConjectureCollatz
     {
         static void Main(string[] args)
         {
+            //declare a string for a pick the number by user
+
+            string a;
+            int result;
+
+
+            Console.WriteLine("Enter a number positive..");
+            a = Console.ReadLine();
+            //convert string to int whit the parse method
+            result = int.Parse(a);
+
+            //while the number is even the cycle will continue and the result module will be made
+            while (result % 2 == 0)
+            {
+                result = result / 2;
+                Console.WriteLine("The new number is: " + result);
+                Console.ReadKey();
+            }
+            while (result != 0)
+            {
+                result = (result * 3) + 1;
+                Console.WriteLine("The new number is: " + result);
+                Console.ReadKey();
+                while (result % 2 == 0)
+                {
+                    result = result / 2;
+                    Console.WriteLine("The new number is: " + result);
+                    Console.ReadKey();
+                }
+            }
+
+
+
         }
+
     }
 }
